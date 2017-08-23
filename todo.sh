418532d0d9f21048672f7379d7ab68dbb6fd89bf
Lev-Arcady Sellem <sellem.dev@gmail.com>
2017-08-24 01:07:56 +0200

    Fix du traitement des options par un case

--- todo.sh
+++ todo.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
-todo(){
-  if [ $# == 0 ]
-  then
-    if [ -s $PWD/.TODO ]
-    then
-      cat -b $PWD/.TODO 2> /dev/null
-    fi
-  else
-    case "$1" in
-      "-r") sed -i "$2d" $PWD/.TODO;;
-      "-g") if [ -z "$2" ]
-            then cat -b ~/.TODO 2> /dev/null;
-            else echo "$*" | sed "s/-g //" >> ~/.TODO
-            fi;;
-      *) echo "$*" >> .TODO;;
-    esac
-  fi
-}
 
-todo
+if [ $# == 0 ]
+then
+  if [ -s $PWD/.TODO ]
+  then
+    cat -b $PWD/.TODO 2> /dev/null
+  fi
+else
+  case "$1" in
+    "-r")
+      sed -i "$2d" $PWD/.TODO
+      ;;
+    "-g")
+      if [ -z "$2" ]; then
+        cat -b ~/.TODO 2> /dev/null
+      else
+        echo "$*" | sed "s/-g //" >> ~/.TODO
+      fi
+      ;;
+    *)
+      echo "$*" >> .TODO
+      ;;
+  esac
+fi
