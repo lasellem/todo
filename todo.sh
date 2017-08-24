3ad95a086e09cb6a56870934e47f24806c73a755
Lucie Le Briquer <lucie.le_briquer@ens-paris-saclay.fr>
2017-08-24 17:48:15 +0200

    Options de priorités pour le TODO général

--- todo.sh
+++ todo.sh
@@ -4,7 +4,7 @@ if [ $# == 0 ]
 then
   if [ -s $PWD/.TODO ]
   then
-    cat -b $PWD/.TODO 2> /dev/null
+    bash ~/Développement/todo/colored.sh $PWD/.TODO 2> /dev/null
   fi
 else
   case "$1" in
@@ -13,13 +13,53 @@ else
       ;;
     "-g")
       if [ -z "$2" ]; then
-        cat -b ~/.TODO 2> /dev/null
+        bash ~/Développement/todo/colored.sh ~/.TODO 2> /dev/null
       else
-        echo "$*" | sed "s/-g //" >> ~/.TODO
+        case "$2" in
+        "-ff") 
+          echo "$*" | sed "s/-g //" | sed "s/-F /0 /" >> ~/.TODO
+        ;;
+        "-f") 
+          echo "$*" | sed "s/-g //" | sed "s/-f /1 /" >> ~/.TODO
+          ;;
+        "-i") 
+          echo "$*" | sed "s/-g //" | sed "s/-i /3 /" >> ~/.TODO
+          ;;
+        "-ii") 
+          echo "$*" | sed "s/-g //" | sed "s/-I /4 /" >> ~/.TODO
+          ;;
+        *)
+          echo "2 $*" | sed "s/-g //" >> ~/.TODO
+          ;;
+        esac
       fi
       ;;
+    "-F") 
+      echo "$*" | sed "s/-F /0 /" >> .TODO
+      ;;
+    "-f") 
+      echo "$*" | sed "s/-f /1 /" >> .TODO
+      ;;
+    "-i") 
+      echo "$*" | sed "s/-i /3 /" >> .TODO
+      ;;
+    "-I") 
+      echo "$*" | sed "s/-I /4 /" >> .TODO
+      ;;
+    "-gF") 
+      echo "$*" | sed "s/-gF /0 /" >> ~/.TODO
+      ;;
+    "-gf") 
+      echo "$*" | sed "s/-gf /1 /" >> ~/.TODO
+      ;;
+    "-gi") 
+      echo "$*" | sed "s/-gi /3 /" >> ~/.TODO
+      ;;
+    "-gI") 
+      echo "$*" | sed "s/-gI /4 /" >> ~/.TODO
+      ;;
     *)
-      echo "$*" >> .TODO
+      echo "2 $*" >> .TODO
       ;;
   esac
 fi
